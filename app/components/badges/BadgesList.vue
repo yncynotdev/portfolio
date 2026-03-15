3cedc902702c9d9d667419ea51a408a0c34f5580
yncy0 <yncyncy0@gmail.com>
2026-03-15 15:30:42 +0800

    fix: BadgesList.vue responsive layout

--- app/components/badges/BadgesList.vue
+++ app/components/badges/BadgesList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-3 grid-rows-1 gap-5 max-w-screen w-full font-silk-screen">
+    <div class="grid grid-rows-3 lg:grid-cols-3 lg:grid-rows-1 gap-5 max-w-screen w-full font-silk-screen">
         <UBadge class="text-md bg-linear-to-r from-primary-500 to-primary-400 text-black">AI-Native</UBadge>
         <UBadge class="text-md bg-linear-to-r from-secondary-600 to-secondary-500 text-black">Frontend</UBadge>
         <UBadge class="text-md bg-linear-to-r from-tertiary-500 to-tertiary-400 text-black">Backend</UBadge>
